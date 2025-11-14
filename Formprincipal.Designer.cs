f31e1bddb6a6c80841b277d3ef3c3eaa9b0cada6
candela melgarejo <candela_melgarejo_86@hotmail.com>
2025-11-14 00:45:42 -0300

    SUCURSALES re diseñoo

--- Formprincipal.Designer.cs
+++ Formprincipal.Designer.cs
@@ -38,13 +38,25 @@
             dateTimePicker1 = new DateTimePicker();
             label4 = new Label();
             textBox1 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox1 = new ComboBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Sitka Small", 26.25F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
-            label1.Location = new Point(236, 9);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(256, 12);
             label1.Name = "label1";
             label1.Size = new Size(222, 52);
             label1.TabIndex = 0;
@@ -54,8 +66,9 @@
             // 
             lblnombresucur.AutoSize = true;
             lblnombresucur.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblnombresucur.ForeColor = SystemColors.ButtonHighlight;
             lblnombresucur.ImageAlign = ContentAlignment.MiddleLeft;
-            lblnombresucur.Location = new Point(12, 97);
+            lblnombresucur.Location = new Point(256, 73);
             lblnombresucur.Name = "lblnombresucur";
             lblnombresucur.Size = new Size(199, 24);
             lblnombresucur.TabIndex = 1;
@@ -63,16 +76,18 @@
             // 
             // txtnbresucursal
             // 
-            txtnbresucursal.Location = new Point(217, 97);
+            txtnbresucursal.Location = new Point(206, 117);
+            txtnbresucursal.Margin = new Padding(3, 4, 3, 4);
             txtnbresucursal.Name = "txtnbresucursal";
-            txtnbresucursal.Size = new Size(127, 23);
+            txtnbresucursal.Size = new Size(308, 22);
             txtnbresucursal.TabIndex = 2;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(46, 144);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(537, 73);
             label2.Name = "label2";
             label2.Size = new Size(134, 24);
             label2.TabIndex = 3;
@@ -80,16 +95,18 @@
             // 
             // txtnumsucursal
             // 
-            txtnumsucursal.Location = new Point(217, 145);
+            txtnumsucursal.Location = new Point(547, 117);
+            txtnumsucursal.Margin = new Padding(3, 4, 3, 4);
             txtnumsucursal.Name = "txtnumsucursal";
-            txtnumsucursal.Size = new Size(88, 23);
+            txtnumsucursal.Size = new Size(100, 22);
             txtnumsucursal.TabIndex = 4;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(599, 144);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(17, 73);
             label3.Name = "label3";
             label3.Size = new Size(162, 24);
             label3.TabIndex = 5;
@@ -98,16 +115,18 @@
             // dateTimePicker1
             // 
             dateTimePicker1.Format = DateTimePickerFormat.Short;
-            dateTimePicker1.Location = new Point(492, 142);
+            dateTimePicker1.Location = new Point(34, 117);
+            dateTimePicker1.Margin = new Padding(3, 4, 3, 4);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(101, 23);
+            dateTimePicker1.Size = new Size(115, 22);
             dateTimePicker1.TabIndex = 6;
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(614, 98);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(318, 175);
             label4.Name = "label4";
             label4.Size = new Size(82, 24);
             label4.TabIndex = 7;
@@ -115,17 +134,127 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(440, 98);
+            textBox1.Location = new Point(415, 177);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(153, 23);
+            textBox1.Size = new Size(166, 22);
             textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(300, 282);
+            label5.Name = "label5";
+            label5.Size = new Size(100, 24);
+            label5.TabIndex = 9;
+            label5.Text = "N° de calle";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(17, 284);
+            label6.Name = "label6";
+            label6.Size = new Size(52, 24);
+            label6.TabIndex = 10;
+            label6.Text = "Calle";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = SystemColors.ButtonHighlight;
+            label7.Location = new Point(17, 228);
+            label7.Name = "label7";
+            label7.Size = new Size(93, 24);
+            label7.TabIndex = 11;
+            label7.Text = "Localidad";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Sitka Small", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = SystemColors.ButtonHighlight;
+            label8.Location = new Point(17, 175);
+            label8.Name = "label8";
+            label8.Size = new Size(90, 24);
+            label8.TabIndex = 12;
+            label8.Text = "Provincia";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(123, 230);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(170, 22);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(415, 282);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(166, 22);
+            textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(123, 284);
+            textBox5.Margin = new Padding(3, 4, 3, 4);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(170, 22);
+            textBox5.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Benos Aires ", "Salta", "Jujuy", "San Juan", "Mendoza", "Chubut", "Rio Negro" });
+            comboBox1.Location = new Point(123, 173);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(170, 26);
+            comboBox1.TabIndex = 17;
+            comboBox1.Text = "Seleccione Provincia";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(166, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(66, 54);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Sitka Small", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(300, 363);
+            button1.Name = "button1";
+            button1.Size = new Size(88, 33);
+            button1.TabIndex = 19;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Formprincipal
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 18F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Brown;
-            ClientSize = new Size(791, 519);
+            ClientSize = new Size(707, 460);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
             Controls.Add(textBox1);
             Controls.Add(label4);
             Controls.Add(dateTimePicker1);
@@ -135,12 +264,14 @@
             Controls.Add(txtnbresucursal);
             Controls.Add(lblnombresucur);
             Controls.Add(label1);
+            Font = new Font("Sitka Small", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(2);
+            Margin = new Padding(2, 3, 2, 3);
             Name = "Formprincipal";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "     Sucursales";
             Load += Formprincipal_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -156,5 +287,15 @@
         private DateTimePicker dateTimePicker1;
         private Label label4;
         private TextBox textBox1;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private ComboBox comboBox1;
+        private PictureBox pictureBox1;
+        private Button button1;
     }
 }
